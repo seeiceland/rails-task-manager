abda57d6caac142b268c20e9fe8c8337443bbf12
moshe sandmann <info@seeiceland.com>
2018-11-12 15:42:42 +0200

    implemneted edit update and create functionality v2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task.permit(:title, :details))
+    params.require(:task).permit(:title, :details)
   end
 
   def set_task
